a6ca6f15c2bc2ac52789dc0e6fd57b83ea07b6e6
Adharsh <contact.adharsh26@gmail.com>
2024-08-24 17:25:14 +0530

    Add files via upload

---
+++ 2390- Removing Stars From a String/removeStars.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i != '*':
+                stack.append(i)
+            else:
+                if stack:
+                    stack.pop()
+        return "".join(stack)
+
+s = "leet**cod*e"
+print(Solution().removeStars(s))
+        
